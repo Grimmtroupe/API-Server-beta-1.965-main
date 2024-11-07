9842d4c0418c083416ca80ff99339db3a5c05fbb
DamienNoel2 <DamienNoel2@hotmail.com>
2024-11-07 15:53:59 -0500

    jsd

--- server.js
+++ server.js
@@ -11,34 +11,4 @@ import RouteRegister from './routeRegister.js';
 RouteRegister.add('GET', 'Bookmarks', 'list');
 
 let server = new APIServer();
-let port = process.env.PORT || 5000;
-
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.post('/api/data', (req, res) => {
-  const { title, text, category, image } = req.body;
-
-  if (!title || !text || !category || !image) {
-      return res.status(400).json({ message: 'Title, text, category, and image are required!' });
-  }
-
-
-  const responseData = {
-      id,           
-      title,      
-      text,         
-      category,    
-      image,       
-      creation,     
-  };
-
-  res.status(200).json(responseData);
-});
-
-
 server.start();
